f6cc8418628eb4b369d911f18fc0355f6f5c69ca
DML <leithdm@gmail.com>
2016-02-03 13:29:17 +0000

    2 buttons added for selecting an image from camera/photoLibrary ImageView included

--- Memento/ViewController.swift
+++ Memento/ViewController.swift
@@ -8,11 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+  
+  @IBOutlet weak var image: UIImageView!
+  @IBOutlet weak var cameraButton: UIBarButtonItem!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+  }
+  
+  override func viewWillAppear(animated: Bool) {
+    cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
   }
 
   override func didReceiveMemoryWarning() {
@@ -20,6 +28,30 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
+  @IBAction func pickImageFromAlbum(sender: UIBarButtonItem) {
+    let imagePicker = UIImagePickerController()
+    imagePicker.delegate = self
+    imagePicker.sourceType = .PhotoLibrary
+    self.presentViewController(imagePicker, animated: true, completion: nil)
+  }
 
+  @IBAction func pickImageFromCamera(sender: UIBarButtonItem) {
+    let imagePicker = UIImagePickerController()
+    imagePicker.delegate = self
+    imagePicker.sourceType = .Camera
+    self.presentViewController(imagePicker, animated: true, completion: nil)
+  }
+  
+  func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+    self.dismissViewControllerAnimated(true, completion: nil)
+  }
+  
+  func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+    if let image = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+      self.dismissViewControllerAnimated(true, completion: nil)
+      self.image.image = image
+    }
+  }
+  
 }
 
