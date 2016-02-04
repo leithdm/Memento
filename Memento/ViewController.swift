a4a9f3827eeed1ab49a4e4f4b520ed34c25049b6
DML <leithdm@gmail.com>
2016-02-04 08:18:56 +0000

    code refactor

--- Memento/ViewController.swift
+++ Memento/ViewController.swift
@@ -8,9 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
+class ViewController: UIViewController {
   
-  @IBOutlet weak var image: UIImageView!
+  @IBOutlet weak var imageView: UIImageView!
   @IBOutlet weak var cameraButton: UIBarButtonItem!
   @IBOutlet weak var topTextField: UITextField!
   @IBOutlet weak var bottomTextField: UITextField!
@@ -18,7 +18,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
   let topTextFieldDelegate = TopTextFieldDelegate()
   let bottomTextFieldDelegate = BottomTextFieldDelegate()
   
-  //MARK: - meme text attributes
+  //meme text attributes
   let memeTextAttributes = [
     NSStrokeColorAttributeName: UIColor.blackColor(), //outline color
     NSForegroundColorAttributeName: UIColor.whiteColor(), //color of text during rendering
@@ -26,24 +26,11 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     NSStrokeWidthAttributeName: -3.0
   ]
   
+  //MARK: - lifecycle methods
   override func viewDidLoad() {
     super.viewDidLoad()
-
-    //set meme attributes
-    bottomTextField.defaultTextAttributes = memeTextAttributes
-    topTextField.defaultTextAttributes = memeTextAttributes
-    
-    //set delegates
-    self.topTextField.delegate = topTextFieldDelegate
-    self.bottomTextField.delegate = bottomTextFieldDelegate
-    
-    //set text
-    topTextField.text = "TOP"
-    bottomTextField.text = "BOTTOM"
-    
-    topTextField.textAlignment = .Center
-    bottomTextField.textAlignment = .Center
-    
+  
+    setMemeAttributes()
   }
   
   override func viewWillAppear(animated: Bool) {
@@ -65,7 +52,28 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     // Dispose of any resources that can be recreated.
   }
   
+  //MARK: - setMemeAttributes
+  func setMemeAttributes() {
+    
+    //set meme default text attributes
+    bottomTextField.defaultTextAttributes = memeTextAttributes
+    topTextField.defaultTextAttributes = memeTextAttributes
+    
+    //set delegates
+    self.topTextField.delegate = topTextFieldDelegate
+    self.bottomTextField.delegate = bottomTextFieldDelegate
+    
+    //set placeholder text
+    topTextField.text = "TOP"
+    bottomTextField.text = "BOTTOM"
+    
+    //set alignment of text
+    topTextField.textAlignment = .Center
+    bottomTextField.textAlignment = .Center
+  }
+  
   //MARK: - keyboard notification methods
+  
   func subscribeToKeyboardWillShowNotification() {
     NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillAppear:", name: UIKeyboardWillShowNotification, object: nil)
   }
@@ -82,6 +90,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     self.view.frame.origin.y += getKeyboardHeight(notification)
   }
   
+  //get keyboard height
   func getKeyboardHeight(notification: NSNotification) -> CGFloat {
     let userInfo = notification.userInfo
     let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue //of CGRect
@@ -98,6 +107,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 
   
   //MARK: - picking an image
+  
+  //pick from photo album
   @IBAction func pickImageFromAlbum(sender: UIBarButtonItem) {
     let imagePicker = UIImagePickerController()
     imagePicker.delegate = self
@@ -105,12 +116,17 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     self.presentViewController(imagePicker, animated: true, completion: nil)
   }
   
+  //pick from the camera
   @IBAction func pickImageFromCamera(sender: UIBarButtonItem) {
     let imagePicker = UIImagePickerController()
     imagePicker.delegate = self
     imagePicker.sourceType = .Camera
     self.presentViewController(imagePicker, animated: true, completion: nil)
   }
+}
+
+//MARK: - UIImagePickerControllerDelegate Extension
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
   
   func imagePickerControllerDidCancel(picker: UIImagePickerController) {
     self.dismissViewControllerAnimated(true, completion: nil)
@@ -119,9 +135,9 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
   func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
     if let image = info["UIImagePickerControllerOriginalImage"] as? UIImage {
       self.dismissViewControllerAnimated(true, completion: nil)
-      self.image.image = image
+      self.imageView.image = image
     }
   }
-  
 }
+
 
