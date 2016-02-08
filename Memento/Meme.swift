3658eea472b7cf1698d66d4ce375553cd725982b
DML <leithdm@gmail.com>
2016-02-08 11:04:44 +0000

    1. Meme changed to type Struct

--- Memento/Meme.swift
+++ Memento/Meme.swift
@@ -9,19 +9,12 @@
 import Foundation
 import UIKit
 
-class Meme {
+struct Meme {
   
   //MARK: - properties
   var topText: String
   var bottomText: String
   var originalImage: UIImage
   var memedImage: UIImage
-
-  init(topText: String, bottomText: String, originalImage: UIImage, memedImage: UIImage) {
-    self.topText = topText
-    self.bottomText = bottomText
-    self.originalImage = originalImage
-    self.memedImage = memedImage
-  }
 	
 }
