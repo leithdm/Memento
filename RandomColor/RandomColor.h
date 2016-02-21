f16e133d576c69c252c426814776cf265d9d90aa
DML <darrenm.leith@lethalapps.com>
2016-02-21 13:46:28 +0000

    .gitignore added to project

--- RandomColor/RandomColor.h
+++ RandomColor/RandomColor.h
@@ -1,6 +1,6 @@
 //
-//  ColorDefinition.swift
-//  RandomColorSwift
+//  RandomColor.h
+//  RandomColor
 //
 //  Copyright (c) 2015 Wei Wang (http://onevcat.com)
 //
@@ -22,28 +22,14 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //  SOFTWARE.
 
-import Foundation
+#import <UIKit/UIKit.h>
 
-typealias Range = (min: Int, max: Int)
+//! Project version number for RandomColor.
+FOUNDATION_EXPORT double RandomColorVersionNumber;
 
-struct ColorDefinition {
-    let hueRange: Range?
-    let lowerBounds: [Range]
-    
-    lazy var saturationRange: Range = {
-        let sMin = self.lowerBounds[0].0
-        let sMax = self.lowerBounds[self.lowerBounds.count - 1].0
-        return (sMin, sMax)
-    }()
-    
-    lazy var brightnessRange: Range = {
-        let bMin = self.lowerBounds[self.lowerBounds.count - 1].1
-        let bMax = self.lowerBounds[0].1
-        return (bMin, bMax)
-    }()
-    
-    init(hueRange: Range?, lowerBounds: [Range]) {
-        self.hueRange = hueRange
-        self.lowerBounds = lowerBounds
-    }
-}
+//! Project version string for RandomColor.
+FOUNDATION_EXPORT const unsigned char RandomColorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RandomColor/PublicHeader.h>
+
+
